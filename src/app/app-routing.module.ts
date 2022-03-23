8fff94e62bc2bede9e656810f97ecae59f786153
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2022-03-23 00:24:19 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { SobrenosComponent } from './shared/components/sobrenos/sobrenos.component';
+import { CardapioComponent } from './shared/components/cardapio/cardapio.component';
+import { ContentComponent } from './shared/components/content/content.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContentComponent},
+  { path: 'home', component: ContentComponent},
+  { path: 'cardapio', component: CardapioComponent},
+  { path: 'sobrenos', component: SobrenosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ContentComponent } from './shared/components/content/content.component';
+import { CardapioComponent } from './shared/components/cardapio/cardapio.component';
+import { SobrenosComponent } from './shared/components/sobrenos/sobrenos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    CardapioComponent,
+    SobrenosComponent
   ],
   imports: [
     BrowserModule,
